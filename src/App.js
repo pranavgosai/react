554e95f362466cf0f46775ec7be74af5e786622f
pranav gosai <115298514+pranavgosai@users.noreply.github.com>
2023-02-17 15:43:03 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
-import React from 'react'
-import {BrowserRouter,Routes,Route} from 'react-router-dom'
-import Login from './pages/Login'
-import Register from './pages/Register'
-import Secret from './pages/Secret'
-export default function App() {
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
   return (
-    <BrowserRouter>
-    <Routes>
-      <Route exact path='/register' element={<Register/>}   />
-      <Route exact path='/Login' element={<Login/>}   />
-      <Route exact path='/' element={<Secret/>}   />
-    </Routes>
-    
-    
-    </BrowserRouter>
-  )
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,59 @@
-import React from 'react';
+
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 
+import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <div>
+   <form class="row g-3">
+  <div class="col-md-6">
+    <label for="inputEmail4" class="form-label">enter your name </label>
+    <input type="email" class="form-control" id="inputEmail4"/>
+  </div>
+  <div class="col-md-6">
+    <label for="inputPassword4" class="form-label">enter your lastname</label>
+    <input type="password" class="form-control" id="inputPassword4"/>
+  </div>
+  <div class="col-12">
+    <label for="inputAddress" class="form-label">age</label>
+    <input type="text" class="form-control" id="inputAddress" placeholder=""/>
+  </div>
+  <div class="col-12">
+    <label for="inputAddress2" class="form-label date">Address 2</label>
+    <input type="text" class="form-control" id="inputAddress2" placeholder=""/>
+  </div>
+  <div class="col-md-6">
+    <label for="inputCity" class="form-label">City</label>
+    <input type="text" class="form-control" id="inputCity"/>
+  </div>
+  <div class="col-md-4">
+    <label for="inputState" class="form-label">State</label>
+    <select id="inputState" class="form-select">
+      <option selected>Choose...</option>
+      <option>...</option>
+    </select>
+  </div>
+  <div class="col-md-2">
+    <label for="inputZip" class="form-label">Zip</label>
+    <input type="text" class="form-control" id="inputZip"/>
+  </div>
+  <div class="col-12">
+    <div class="form-check">
+      <input class="form-check-input" type="checkbox" id="gridCheck"/>
+      <label class="form-check-label" for="gridCheck">
+        Check me out
+      </label>
+    </div>
+  </div>
+  <div class="col-12">
+    <button type="submit" class="btn btn-primary">Sign in</button>
+  </div>
+</form>
+</div>
+ 
 );
 
 
+reportWebVitals();
