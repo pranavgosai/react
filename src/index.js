6d1295349939b787b42788436561b55fde6c24df
pranav gosai <115298514+pranavgosai@users.noreply.github.com>
2023-02-21 10:21:06 +0530

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,59 +1,74 @@
-
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-
-import reportWebVitals from './reportWebVitals';
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <div>
-   <form class="row g-3">
-  <div class="col-md-6">
-    <label for="inputEmail4" class="form-label">enter your name </label>
-    <input type="email" class="form-control" id="inputEmail4"/>
-  </div>
-  <div class="col-md-6">
-    <label for="inputPassword4" class="form-label">enter your lastname</label>
-    <input type="password" class="form-control" id="inputPassword4"/>
-  </div>
-  <div class="col-12">
-    <label for="inputAddress" class="form-label">age</label>
-    <input type="text" class="form-control" id="inputAddress" placeholder=""/>
-  </div>
-  <div class="col-12">
-    <label for="inputAddress2" class="form-label date">Address 2</label>
-    <input type="text" class="form-control" id="inputAddress2" placeholder=""/>
-  </div>
-  <div class="col-md-6">
-    <label for="inputCity" class="form-label">City</label>
-    <input type="text" class="form-control" id="inputCity"/>
-  </div>
-  <div class="col-md-4">
-    <label for="inputState" class="form-label">State</label>
-    <select id="inputState" class="form-select">
-      <option selected>Choose...</option>
-      <option>...</option>
-    </select>
-  </div>
-  <div class="col-md-2">
-    <label for="inputZip" class="form-label">Zip</label>
-    <input type="text" class="form-control" id="inputZip"/>
-  </div>
-  <div class="col-12">
-    <div class="form-check">
-      <input class="form-check-input" type="checkbox" id="gridCheck"/>
-      <label class="form-check-label" for="gridCheck">
-        Check me out
-      </label>
-    </div>
-  </div>
-  <div class="col-12">
-    <button type="submit" class="btn btn-primary">Sign in</button>
-  </div>
-</form>
-</div>
- 
-);
-
-
-reportWebVitals();
+function PlayerDetail(player)
+{
+    return (
+      <div className='container mt-5'>
+        <div className='row'>
+            <div className='col-6 offset-3'>
+                <div className='card shadow'>
+                    <div className='card-header'>
+                        <h3>{player.name}</h3>
+                    </div>
+                    <div className='card-body'>
+                        <table className='table table-bordered'>
+                            <tr>
+                                <td width='25%'>dob</td>
+                                <td>{player.dob}</td>
+                            </tr>
+                            <tr>
+                                <td>gender</td>
+                                <td>{player.gender}</td>
+                            </tr>
+                            <tr>
+                                <td>bloodgroup</td>
+                                <td>{player.bloodgroup}</td>
+                            </tr>
+                            <tr>
+                                <td width='25%'>mobile</td>
+                                <td>{player.mobile}</td>
+                            </tr>
+                            <tr>
+                                <td width='25%'>email</td>
+                                <td>{player.email}</td>
+                            </tr>
+                            <tr>
+                                <td width='25%'>weight</td>
+                                <td>{player.weight}</td>
+                            </tr>
+                            <tr>
+                                <td width='25%'>height</td>
+                                <td>{player.height}</td>
+                            </tr>
+                            <tr>
+                                <td width='25%'>hobbies</td>
+                                <td>{player.hobbies}</td>
+                            </tr>
+                            <tr>
+                                <td width='25%'>education</td>
+                                <td>{player.education}</td>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+      </div>
+    );
+}
+//create javascript object 
+let player = {
+  name:'pranav',
+  dob:'22-06-1999',
+  gender:'male',
+  bloodgroup:'b+',
+  mobile:'123456789',
+  email:'pranav@gmail.com',
+  weight:'53',
+  height:'5.6',
+  hobbies:'music',
+  education:'b.com',
+}
+root.render(PlayerDetail(player));
