96cda4dc57aba4c6644d22829270b1bba4f72551
pranav gosai <115298514+pranavgosai@users.noreply.github.com>
2023-03-09 12:06:23 +0530

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,20 +1,58 @@
-
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import Teacher from './index1';
-import './index.css';
-const root = ReactDOM.createRoot(document.getElementById('root'));
-function Team()
-{
-    return (
-        <div className='container'>
-            <div className='row'>
-                <Teacher name="pranav" age='38' />
-                <Teacher name="shivani" age='18' />
-                <Teacher name="bob" age='25' />
-                
-            </div>
-        </div>
-    )
+
+function DisplayCurrentDate() {
+  const currentDate = new Date();
+  const day = currentDate.getDate();
+  const month = currentDate.getMonth();
+  const year = currentDate.getFullYear();
+  
+  let monthName;
+  switch(month) {
+    case 0:
+      monthName = "january";
+      break;
+    case 1:
+      monthName = "february";
+      break;
+    case 2:
+      monthName = "march";
+      break;
+    case 3:
+      monthName = "april";
+      break;
+    case 4:
+      monthName = "may";
+      break;
+    case 5:
+      monthName = "june";
+      break;
+    case 6:
+      monthName = "july";
+      break;
+    case 7:
+      monthName = "august";
+      break;
+    case 8:
+      monthName = "september";
+      break;
+    case 9:
+      monthName = "october";
+      break;
+    case 10:
+      monthName = "november";
+      break;
+    case 11:
+      monthName = "december";
+      break;
+    default:
+      monthName = "";
+      break;
+  }
+  
+  const formattedDate = `${day}-${monthName}-${year}`;
+  
+  return (
+    <p>{formattedDate}</p>
+  );
 }
-root.render(<Team />);
+
