fc073fa09725f095cf5f459ed95fcb96701879e1
pranav gosai <115298514+pranavgosai@users.noreply.github.com>
2023-03-14 22:02:07 +0530

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,34 +1,38 @@
 import React,{useState} from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Todoinput from './componentes/Todoinput';
-import Todolist from './componentes/Todolist';
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 function Resturant(){
-    const [listTodo,setListTodo]= useState([]);
-    let addList = (inputText) =>{
-        if(inputText!=='')
-        setListTodo([...listTodo,inputText]);
-    }
-    const deletListItem=(key)=>{
-        let newListTodo = [...listTodo];
-        newListTodo.splice(key,1)
-        setListTodo([...newListTodo])
-    }
+   function add(a,b){
+    const sum = a+b;
+    return sum;
+   }
+   function sub(a,b){
+    const sub = a-b;
+    
+    return sub;
+   }
+   function div(a,b){
+    const div = a/b;
+   
+    return div;
+   }
+   function mult(a,b){
+    const mult = a*b;
+    return mult;
+   }
+   
     return(
-    <div className="main-container">
-        <div className="center-container">
-            <Todoinput addList={addList}/>
-            <h1 className='app-heading'>TODO</h1>
-            <hr />
-            {listTodo.map((listItem,i)=>{
-                return(
-                    <Todolist key={i} index={i} item={listItem} deletItem={deletListItem}/>
-                )
-            })}
-
-        </div>
+    <div>
+        <ol className=' text-center mt-5'>
+        <h1>sum of tow no is {add(40,10)}</h1>
+        <h1>sub of tow no is {sub(40,10)}</h1>
+        <h1>div of tow no is {div(40,7)}</h1>
+        <h1>mult of tow no is {mult(40,10)}</h1>
+      </ol>
     </div>
     
     );
