4258e48f2febd2d66bb907038f828223f3655a7c
pranav gosai <115298514+pranavgosai@users.noreply.github.com>
2023-03-15 11:26:00 +0530

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,40 +1,26 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 
 
+function LoginForm()
+{
+    //create state variable using useState
+    let time = new Date().toLocaleTimeString();
+    const [ctime,setCtime] = useState(time);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-function Resturant(){
-   function add(a,b){
-    const sum = a+b;
-    return sum;
-   }
-   function sub(a,b){
-    const sub = a-b;
-    
-    return sub;
-   }
-   function div(a,b){
-    const div = a/b;
-   
-    return div;
-   }
-   function mult(a,b){
-    const mult = a*b;
-    return mult;
-   }
-   
-    return(
-    <div>
-        <ol className=' text-center mt-5'>
-        <h1>sum of tow no is {add(40,10)}</h1>
-        <h1>sub of tow no is {sub(40,10)}</h1>
-        <h1>div of tow no is {div(40,7)}</h1>
-        <h1>mult of tow no is {mult(40,10)}</h1>
-      </ol>
+    const UpdateTime = () =>{
+        time = new Date().toLocaleTimeString();
+        setCtime(time);
+
+    }
+
+    setInterval(UpdateTime,1000)
+  
+    return (<div className='canteiner text-center mt-5'>
+        <h1>{ctime}</h1>
     </div>
-    
+
     );
 }
-root.render(<Resturant />);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<LoginForm />);
