d71a1c54e45d04c2ceb52bde94c88078aa4c4c90
pranav gosai <115298514+pranavgosai@users.noreply.github.com>
2023-03-18 12:07:35 +0530

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,60 +1,19 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom/client';
 
 function LoginForm()
 {
     //create state variable using useState
-    var [message,setMessage] = useState('here we will display message');
-    var [email,setEmail] = useState('');
-    var [password ,setPassword]=useState('');
+   const [count,setCount]= useState(0);
+   const [time,setTime]=useState(new Date());
+    useEffect(()=>{
+        setTime(new Date());
+    })
 
-
-    var SubmitForm =(event)=> {
-
-        event.preventDefault();
-        console.log(email);
-        console.log(password);
-        if(email=="admin@gmail.com" && password == "123123")
-        {
-            setMessage("login successfull");
-            
-        }
-        else{
-            setMessage("invalid login attempt");
-            
-        }
-    }
     return (<div>
-        <div className='container'>
-        <div className='row'>
-            <div className='col-lg-8 offset-2'>
-                <div className='card shadow'>
-                    <div className='card-header text-bg-dark'>
-                        <h3>Login</h3>
-                    </div>
-                    <div className='card-body'>
-                        <form onSubmit={SubmitForm}>
-                            <div className='mb-3'>
-                                <label htmlFor='email'>Email</label>
-                                <input className='form-control' id='email' onChange={(event)=>setEmail(event.target.value)}
-                                placeholder='Email' type='email' value={email} />
-                            </div>
-                            <div className='mb-3'>
-                                <label htmlFor='password'>Password</label> 
-                                <input className='form-control' id='password' onChange={(event) => setPassword(event.target.value)} placeholder='Password' type='password' value={password} />
-                            </div>
-                            <div className='mb-3'>
-                                <input type='submit' value='Login' className='btn btn-danger'/>
-                            </div>
-                            <div className='text-bg-dark p-2 text-center'>{message}</div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-       
- 
+
+        <h1>current time: {time.toUTCString()}</h1>
+      
        
     </div>
 
