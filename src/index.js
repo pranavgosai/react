478c4d6f37805a768cf51116624a5c211a2ee8ed
pranav gosai <115298514+pranavgosai@users.noreply.github.com>
2023-03-19 13:46:19 +0530

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -4,19 +4,44 @@ import ReactDOM from 'react-dom/client';
 function LoginForm()
 {
     //create state variable using useState
-   const [count,setCount]= useState(0);
-   const [time,setTime]=useState(new Date());
-    useEffect(()=>{
-        setTime(new Date());
-    })
+    var [msg,setMsg]=useState('hi');
+   var [email,setEmail]=useState('');
+   var [password,setPassword]=useState('');
+   var [count,setCount] = useState(1);
+   
+   
+  const SubmitForm =(e)=>{
+    e.preventDefault();
+    console.log(email);
+    console.log(password);
+    if(email=='pranavgosai01@gmail.com'&&password=='123')
+    
+    {
+      setMsg("loghin ok");
+    }else{
+      setMsg("no hale");
+      setCount(count+1);
+    }if(count==3)
+    {
+        setMsg('aala revadene');
+    }
 
-    return (<div>
+  }
 
-        <h1>current time: {time.toUTCString()}</h1>
-      
-       
+    return (
+      <div className="card" >
+        <form onSubmit={SubmitForm}>
+      <h5 className="card-header">login form</h5>
+      <div className="card-body">
+        <h5 className="card-title">email</h5>
+        <input type="email" placeholder='email' id='email'  value={email} onChange={(e)=>setEmail(e.target.value)}  /><br />
+        <h5 className="card-title">password</h5>
+        <input type="password" placeholder='password' id='password' value={password} onChange={(e)=>setPassword(e.target.value)} /> <br />
+        <button href="#" type='submit' className="btn btn-primary mt-2">login</button>
+        <h5>{msg}</h5>
+      </div>
+      </form>
     </div>
-
     );
 }
 const root = ReactDOM.createRoot(document.getElementById('root'));
