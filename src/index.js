9a74aeed6e9f1c6800c9d6d3d0e4a5ec28672ff3
pranav gosai <115298514+pranavgosai@users.noreply.github.com>
2023-03-20 14:05:59 +0530

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,47 +1,46 @@
 import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css';
 
 function LoginForm()
 {
     //create state variable using useState
-    var [msg,setMsg]=useState('hi');
-   var [email,setEmail]=useState('');
-   var [password,setPassword]=useState('');
-   var [count,setCount] = useState(1);
    
-   
-  const SubmitForm =(e)=>{
-    e.preventDefault();
-    console.log(email);
-    console.log(password);
-    if(email=='pranavgosai01@gmail.com'&&password=='123')
-    
-    {
-      setMsg("loghin ok");
-    }else{
-      setMsg("no hale");
-      setCount(count+1);
-    }if(count==3)
-    {
-        setMsg('aala revadene');
-    }
+   const ctrateDigts = ()=>{
+    const digits = [];
+    for (let i = 1; i < 10; i++){
+      digits.push(
+        <button key={i}>{i}</button>
+      )
 
-  }
+    }
+    return digits;
+   }
+
+   
+ 
+
+  
 
     return (
-      <div className="card" >
-        <form onSubmit={SubmitForm}>
-      <h5 className="card-header">login form</h5>
-      <div className="card-body">
-        <h5 className="card-title">email</h5>
-        <input type="email" placeholder='email' id='email'  value={email} onChange={(e)=>setEmail(e.target.value)}  /><br />
-        <h5 className="card-title">password</h5>
-        <input type="password" placeholder='password' id='password' value={password} onChange={(e)=>setPassword(e.target.value)} /> <br />
-        <button href="#" type='submit' className="btn btn-primary mt-2">login</button>
-        <h5>{msg}</h5>
+     <div className="calculator">
+      <div className="display">
+        <span>(0)</span> 0
       </div>
-      </form>
-    </div>
+      <div className="operators">
+        <button>/</button>
+        <button>*</button>
+        <button>+</button>
+        <button>-</button>
+        <button>DEl</button>
+      </div>
+      <div className="digits">
+        {ctrateDigts()}
+        <button>0</button>
+        <button>.</button>
+        <button>=</button>
+      </div>
+     </div>
     );
 }
 const root = ReactDOM.createRoot(document.getElementById('root'));
