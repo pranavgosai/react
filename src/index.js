f39af90072533d81722bd3b8ce11b48f065aceed
pranav gosai <115298514+pranavgosai@users.noreply.github.com>
2023-04-01 18:13:39 +0530

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,47 +1,28 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-
-function LoginForm()
-{
-    //create state variable using useState
-   
-   const ctrateDigts = ()=>{
-    const digits = [];
-    for (let i = 1; i < 10; i++){
-      digits.push(
-        <button key={i}>{i}</button>
-      )
-
-    }
-    return digits;
-   }
-
-   
- 
-
-  
-
-    return (
-     <div className="calculator">
-      <div className="display">
-        <span>(0)</span> 0
-      </div>
-      <div className="operators">
-        <button>/</button>
-        <button>*</button>
-        <button>+</button>
-        <button>-</button>
-        <button>DEl</button>
-      </div>
-      <div className="digits">
-        {ctrateDigts()}
-        <button>0</button>
-        <button>.</button>
-        <button>=</button>
-      </div>
-     </div>
-    );
-}
+import Layout from './Layout';
+import Home from './Home';
+import AboutUs from './AboutUs';
+import ContactUs from './ContactUs';
+import Product from './Product'
+import Course from './Course'
+import { BrowserRouter,Routes, Route } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<LoginForm />);
+function MyRouter()
+{
+    return(
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<Layout />}>
+                    <Route path='/home' element={<Home />} />
+                    <Route path='/aboutus' element={<AboutUs />} />
+                    <Route path='/product' element={<Product />} />
+                    <Route path='/courses' element={<Course />} />
+                    <Route path='/contact' element={<ContactUs />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    )
+}
+root.render(<MyRouter />);
