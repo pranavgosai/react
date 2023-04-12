28394157815c88d8f8de764956e21d9bb0ca1676
pranav gosai <115298514+pranavgosai@users.noreply.github.com>
2023-04-12 13:58:09 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,30 +1,53 @@
-const  express = require ("express");
-
-const cors =require("cors");
-const mongoose = require("mongoose");
-const authRoutes = require("./Routes/AuthRoutes");
-const app = express();
-const cookieParser = require("cookie-parser")
-
-app.listen(4000,()=>{
-    console.log("server started on PORT 4000");
-});
-mongoose.set(`strictQuery`,true);
-mongoose.connect("mongodb://0.0.0.0:27017/user",{
-    useNewUrlparser:true, 
-    useUnifiedTopology:true,
-}).then(()=>{
-    console.log("DB connection succsessfull");
-}).catch(err=>{
-    console.log(err.message);
-})
-
-app.use(cors({
-    origin:["http://localhost:3000"],
-    method:["GET" , "POST"],
-    credentials:true,
-}))
-
-app.use(cookieParser());
-app.use(express.json());
-app.use("/",authRoutes);
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+function Calculator()
+{
+    const [num1, setNum1] = useState("");
+    const [num2, setNum2] = useState("");
+    const [result, setResult] = useState("");
+  
+    const handleNum1Change = (event) => {
+      setNum1(event.target.value);
+    };
+  
+    const handleNum2Change = (event) => {
+      setNum2(event.target.value);
+    };
+  
+    const handleAddition = () => {
+      setResult(Number(num1) + Number(num2));
+    };
+  
+    const handleSubtraction = () => {
+      setResult(Number(num1) - Number(num2));
+    };
+  
+    const handleMultiplication = () => {
+      setResult(Number(num1) * Number(num2));
+    };
+  
+    const handleDivision = () => {
+      setResult(Number(num1) / Number(num2));
+    };
+    return(
+        <div>
+      <div>
+        <input type="number" value={num1} onChange={handleNum1Change} />
+        <input type="number" value={num2} onChange={handleNum2Change} />
+      </div>
+      <div>
+        <button onClick={handleAddition}>+</button>
+        <button onClick={handleSubtraction}>-</button>
+        <button onClick={handleMultiplication}>*</button>
+        <button onClick={handleDivision}>/</button>
+      </div>
+      <div>
+        <input type="number" value={result} readOnly />
+      </div>
+    </div>
+       
+    )
+}
+root.render(<Calculator />);
